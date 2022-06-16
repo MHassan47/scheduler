b81bb3eebbd88504df0b157a0f6f24fa1d14abb3
Mohamed <mhassankd35@gmail.com>
2022-06-16 10:39:53 -0500

    implements appointment index, header and empty

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+// import Appointment from "components/Appointment";
+import "components/Appointment/styles.scss";
+
+const Appointment = function (props) {
+  return <article className="appointment"></article>;
+};
+
+export default Appointment;

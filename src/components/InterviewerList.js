ec1e8dc20e2cf06d48584217197746f1f8bf47c1
Mohamed <mhassankd35@gmail.com>
2022-06-16 07:39:28 -0500

    implements InterviewerList component that holds all individual interviewers + styling for hover and select + addition to story

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import InterviewerListItem from "./InterviewerListItem";
+import "components/InterviewerList.scss";
+
+const InterviewerList = function (props) {
+  const interviewer = props.interviewers.map((interviewer) => {
+    return (
+      <section className="interviewers">
+        <ul className="interviewers__list" key={interviewer.id}>
+          <InterviewerListItem
+            name={interviewer.name}
+            avatar={interviewer.avatar}
+            selected={props.interviewer === interviewer.id}
+            setInterviewer={() => props.setInterviewer(interviewer.id)}
+          />
+        </ul>
+      </section>
+    );
+  });
+  return interviewer;
+};
+
+export default InterviewerList;

fdcd87be360c9c8359e033ad75b95b4bc1413106
Mohamed <mhassankd35@gmail.com>
2022-06-20 00:05:28 -0500

    adds getInterviewer helper function and test

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,15 +1,37 @@
 const getAppointmentsForDay = function (state, day) {
   const appointmentByDay = state.days.find((eachDay) => eachDay.name === day);
 
-  console.log({ appointmentByDay });
+  //   console.log({ appointmentByDay });
   if (!appointmentByDay || !appointmentByDay.appointments) {
     return [];
   }
   const appointments = appointmentByDay.appointments.map(
     (appointmentID) => state.appointments[appointmentID]
   );
-  console.log(appointments);
+  //   console.log(appointments);
   return appointments;
 };
 
-export { getAppointmentsForDay };
+const state = {
+  1: {
+    id: 1,
+    name: "Sylvia Palmer",
+    avatar: "https://i.imgur.com/LpaY82x.png",
+  },
+  2: {
+    id: 2,
+    name: "Tori Malcolm",
+    avatar: "https://i.imgur.com/Nmx0Qxo.png",
+  },
+};
+
+function getInterview(state, interview) {
+  if (interview) {
+    const interviewer = state.interviewers[interview.interviewer];
+
+    return { ...interview, interviewer };
+  }
+  return null;
+}
+
+export { getAppointmentsForDay, getInterview };

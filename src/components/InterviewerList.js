65e608280c5eca97e2ca91cc6900c7161acc40b9
Mohamed <mhassankd35@gmail.com>
2022-06-22 01:31:46 -0500

    fixes elements so interviewers list is ordered in row

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,24 +3,22 @@ import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss";
 
 const InterviewerList = function (props) {
-  const interviewer = props.interviewers.map((interviewer) => {
-    return (
-      <section className="interviewers">
-        <ul className="interviewers__list">
-          <InterviewerListItem
-            key={interviewer.id}
-            name={interviewer.name}
-            avatar={interviewer.avatar}
-            selected={props.value === interviewer.id}
-            setInterviewer={() => {
-              props.onChange(interviewer.id);
-            }}
-          />
-        </ul>
-      </section>
-    );
-  });
-  return interviewer;
+  const interviewer = props.interviewers.map((interviewer) => (
+    <InterviewerListItem
+      key={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected={props.value === interviewer.id}
+      setInterviewer={() => {
+        props.onChange(interviewer.id);
+      }}
+    />
+  ));
+  return (
+    <section className="interviewers">
+      <ul className="interviewers__list">{interviewer}</ul>
+    </section>
+  );
 };
 
 export default InterviewerList;
